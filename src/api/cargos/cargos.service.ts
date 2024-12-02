03f1a64ff253f3e6bacf14fac7b863235f5a9ba4
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-02 19:03:22 -0300

    Criando novos metodos para tabela CARGOS

--- src/api/cargos/cargos.service.ts
+++ src/api/cargos/cargos.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 import { CreateCargoDto } from './dto/create-cargo.dto';
 import { UpdateCargoDto } from './dto/update-cargo.dto';
 import { prismaConfig } from 'src/config/prismaConfig';
-import { Cargo } from './entities/cargo.entity';
 
 const { cargos } = prismaConfig
 
@@ -39,19 +38,91 @@ export class CargosService {
     }
   }
 
-  findAll() {
-    return `This action returns all cargos`;
+  async ListarCargos() {
+    try {
+      const cargosCadastrados = await cargos.findMany()
+
+      if (!cargosCadastrados) {
+        return "Não existe nenhum cargo cadastrado no sistema."
+      }
+
+      return cargosCadastrados
+
+    } catch (error) {
+      return 'Erro ao buscar os dados dos cargos cadastrados no sistema, por favor tente novamente.'
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} cargo`;
+  async BuscarCargoID(id: number) {
+    try {
+      const cargoID = await cargos.findFirst({ where: { id } })
+
+      if (cargoID) {
+        return cargoID
+      }
+
+      return `Não existe nenhum cargo cadastrado com o ID=${id}`
+
+    } catch (error) {
+      return "Não conseguimos realizar a consulta no banco de dados,por favor tente novamente."
+    }
   }
 
-  update(id: number, updateCargoDto: UpdateCargoDto) {
-    return `This action updates a #${id} cargo`;
+  // async BuscarCargoNome(cargo: string) {
+  //   try {
+  //     const cargoNome = await cargos.findFirst({ where: { cargo }})
+
+  //     if(cargoNome) {
+  //       return cargoNome
+  //     }
+
+  //     return `Não existe nenhum cargo cadastrado com o nome=${cargo}`
+
+  //   } catch (error) {
+  //     return "Não conseguimos realizar a consulta no banco de dados,por favor tente novamente."
+  //   }
+  // }
+
+  async AtualizarCargo(id: number, updateCargoDto: UpdateCargoDto) {
+
+    const { cargo } = updateCargoDto
+
+    try {
+      const idCargo = await cargos.findFirst({ where: { id } })
+
+      if(cargo === idCargo.cargo) {
+        return "O nome do cargo informado, é o mesmo que ja esta cadastrado."
+      }
+
+      if (idCargo) {
+        const editar = await cargos.update({
+          where: { id },
+          data: { cargo }
+        })
+
+        return {
+          "status":"Os dados foram atualizados com sucesso.",
+          "dados_antigos": idCargo,
+          "dados_atualizados": editar
+        }
+      }
+    } catch (error) {
+      return `Erro ao tentar atualizar os dados do cargo com ID=${id}`
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} cargo`;
+  async ApagarCargo(id: number) {
+    try {
+      const cargoID = await cargos.findFirst({where: {id}})
+
+      if(cargoID) {
+        await cargos.delete({ where: {id}})
+        return "O cargo foi excluído como solicitado."
+      }
+
+      return `Não foi encontrado nenhum cargo com o ID=${id}`
+    } catch (error) {
+      return "Tivemos um erro ao tentar buscar os dados do cargo solicitado."
+    }
   }
 }
