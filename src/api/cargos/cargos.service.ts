baa2fefd1017e2417f3ef68af0af4ba98b4f0fab
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-02 18:30:30 -0300

    Criando o metodo para cadastrar um novo cargo no sistema

--- src/api/cargos/cargos.service.ts
+++ src/api/cargos/cargos.service.ts
@@ -1,11 +1,42 @@
+import { Cargos } from './../../../node_modules/.prisma/client/index.d';
 import { Injectable } from '@nestjs/common';
 import { CreateCargoDto } from './dto/create-cargo.dto';
 import { UpdateCargoDto } from './dto/update-cargo.dto';
+import { prismaConfig } from 'src/config/prismaConfig';
+import { Cargo } from './entities/cargo.entity';
+
+const { cargos } = prismaConfig
 
 @Injectable()
 export class CargosService {
-  create(createCargoDto: CreateCargoDto) {
-    return 'This action adds a new cargo';
+  async CriarCargo(createCargoDto: CreateCargoDto) {
+
+    const { cargo } = createCargoDto
+
+    try {
+      const cargoExistente = await cargos.findFirst({
+        where: {
+          cargo: {
+            equals: cargo,
+            mode: "insensitive"
+          }
+        }
+      })
+
+      if (cargoExistente) {
+        return `O cargo ${cargoExistente.cargo.toUpperCase()} já esta cadastrado no sistema.`
+      }
+
+      const criar = await cargos.create({
+        data: {
+          cargo
+        }
+      })
+
+      return `O cargo ${criar.cargo.toUpperCase()} foi criado com sucesso.`
+    } catch (error) {
+      return 'Erro ao criar cargo, por favor tente novamente.'
+    }
   }
 
   findAll() {
