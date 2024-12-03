7064569b349f4d86cca69ab10ce1341f999b7994
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-03 11:45:08 -0300

    Criando configurações do modelo de DTO ( criação e edição ) do USUÁRIO

--- src/api/usuarios/dto/update-usuario.dto.ts
+++ src/api/usuarios/dto/update-usuario.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUsuarioDto } from './create-usuario.dto';
 
-export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {}
+export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {
+    nome: string;
+    sobrenome?: string;
+    senha: string;
+}
--- src/api/usuarios/entities/usuario.entity.ts
+++ src/api/usuarios/entities/usuario.entity.ts
@@ -1,1 +1,9 @@
-export class Usuario {}
+export class Usuario {
+    id: string;
+    nome: string;
+    sobrenome?: string;
+    email: string;
+    senha: string;
+    dt_criacao: Date;
+    dt_atualizacao: Date;
+}
