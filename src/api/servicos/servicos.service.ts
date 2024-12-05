6db22b39f27eaff818c9802c9c2e6f490c85a7df
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-05 09:54:44 -0300

    Finalizando os metodos dos Serviços

--- src/api/servicos/servicos.service.ts
+++ src/api/servicos/servicos.service.ts
@@ -15,11 +15,11 @@ export class ServicosService {
 
       id = Math.floor(Math.random() * 10000) + 1
 
-      if(id === servicoJaCadastrado.id) {
+      if (id === servicoJaCadastrado.id) {
         id = servicoJaCadastrado.id * 2
       }
 
-      if(!servicoJaCadastrado) {
+      if (!servicoJaCadastrado) {
         const cadastrar = await servicos.create({
           data: {
             id, tipo, preco
@@ -33,19 +33,87 @@ export class ServicosService {
     }
   }
 
-  findAll() {
-    return `This action returns all servicos`;
+  async ListarServicos() {
+    try {
+      const listaDeServicos = await servicos.findMany()
+
+      if (!listaDeServicos) {
+        return "Não existe nenhum serviço cadastrado no sistema."
+      }
+
+      return listaDeServicos
+    } catch (error) {
+      return "Erro interno! Não conseguimos realizar a consulta dos serviços no sistema. Por favor, tente novamente."
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} servico`;
+  async ServicoID(id: number) {
+    try {
+      const idServico = await servicos.findFirst({ where: { id } })
+
+      if (!idServico) {
+        return `Não existe nenhum serviço com o ID=${id} que foi informado.`
+      }
+
+      return idServico
+
+    } catch (error) {
+      return "Erro interno! Não conseguimos realizar a consulta dos serviços no sistema. Por favor, tente novamente."
+    }
   }
 
-  update(id: number, updateServicoDto: UpdateServicoDto) {
-    return `This action updates a #${id} servico`;
+  async ServicoNome(tipo: string) {
+    try {
+      const nomeServico = await servicos.findFirst({ where: { tipo } })
+
+      if (!nomeServico) {
+        return `Não existe nenhum serviço com o nome ${tipo} cadastrado no sistema.`
+      }
+
+      return nomeServico
+
+    } catch (error) {
+      return "Erro interno! Não conseguimos realizar a consulta dos serviços no sistema. Por favor, tente novamente."
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} servico`;
+  async EditarServico(id: number, updateServicoDto: UpdateServicoDto) {
+    try {
+      const { tipo, preco } = updateServicoDto;
+
+      const servicoId = await servicos.findFirst({ where: { id } })
+
+      if(servicoId) {
+        const editado = await servicos.update({
+          where: {id},
+          data: {
+            tipo: tipo.trim() === "" ? servicoId.tipo : tipo,
+            preco: preco === 0 ? servicoId.preco : preco
+          }
+        })
+
+        return {
+          status: "Os dados foram atualizados com sucesso.",
+          dados_antigos: servicoId,
+          dados_atualizados: editado
+        }
+      }
+    } catch (error) {
+      return "Erro interno! Não conseguimos realizar a consulta dos serviços no sistema para realizar as atualizações dos dados. Por favor, tente novamente."
+    }
+  }
+
+  async Apagar(id: number) {
+    try {
+      const idServidoExistente = await servicos.findFirst({ where: { id }})
+      if(idServidoExistente) {
+        await servicos.delete({ where: { id }})
+        return `Os dados do servico ${idServidoExistente.tipo} foram excluídos com sucesso.`
+      } 
+
+      return `Não foi encontrado nenhum servico com o ID=${id}`
+    } catch (error) {
+      return "Erro interno! Por favor, tente novamente."
+    }
   }
 }
