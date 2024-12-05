ca5e9ea997a9525b9eba3b5034669520524b2f0c
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-05 18:43:05 -0300

    Adicionando teste de middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FormUsuario } from './middlewares/FormUsuario'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(new FormUsuario())
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
