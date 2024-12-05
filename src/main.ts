3c59ef52bb908c461fbb173da82b37ab3cfc5b37
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-05 18:45:10 -0300

    Removendo o teste de middleware

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { FormUsuario } from './middlewares/FormUsuario'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use(new FormUsuario())
 
   await app.listen(process.env.PORT ?? 3000);
 }
