7b36f4563f25754195770c68bee69e2aa86eaaea
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-03 10:00:48 -0300

    Removendo linha de codigo desnecessário

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { UsuariosModule } from './api/usuarios/usuarios.module';
 import { CargosModule } from './api/cargos/cargos.module';
 import { ServicosModule } from './api/servicos/servicos.module';
 import { AgendasModule } from './api/agendas/agendas.module';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [UsuariosModule, CargosModule, ServicosModule, AgendasModule],
