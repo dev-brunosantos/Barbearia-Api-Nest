ab34800b999931e6d8965cd139c9d297821c47c3
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-03 10:01:47 -0300

    Adicionando o modulo do prisma para realizar a conexão como banco de dados

--- src/api/usuarios/usuarios.module.ts
+++ src/api/usuarios/usuarios.module.ts
@@ -1,8 +1,10 @@
+import { PrismaModule } from './../../prisma/prisma.module';
 import { Module } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UsuariosController],
   providers: [UsuariosService],
 })
