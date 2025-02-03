f8aa061c3cd40786e73ac1152379c7536f8f66bb
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-03 11:26:12 -0300

    Refatorando o codigo e excluindo linhas desnecessárias

--- src/api/usuarios/usuarios.service.ts
+++ src/api/usuarios/usuarios.service.ts
@@ -1,13 +1,9 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
-import { prismaConfig } from 'src/config/prismaConfig';
+import { PrismaService } from './../../prisma/prisma.service';
 import { formatarDataISO } from 'src/functions/FormataData';
 import { FormataCargo } from 'src/functions/FormataCargo';
-
-import { PrismaService } from './../../prisma/prisma.service';
-
-const { usuarios } = prismaConfig;
 
 @Injectable()
 export class UsuariosService {
@@ -24,7 +20,7 @@ export class UsuariosService {
 
       let cargo = FormataCargo(createUsuarioDto.tipoCargo) //TESTANDO FUNÇÃO DINAMICA PARA VALIDAR FUNÇÃO DO USUÁRIO
 
-      const novoUsuario = this.prisma.usuarios.create({
+      const novoUsuario = await this.prisma.usuarios.create({
         data: {
           nome: createUsuarioDto.nome,
           sobrenome: createUsuarioDto.sobrenome,
@@ -34,34 +30,12 @@ export class UsuariosService {
         }
       })
 
-      return "Novo usuário cadastrado com sucesso."
+      return `O usuário ${novoUsuario.nome.toUpperCase()} ${novoUsuario.sobrenome.toUpperCase()} foi cadastrado com sucesso.`
     }
     throw new HttpException("O email informado já esta vinculado a outro usuário no sistema.", HttpStatus.BAD_REQUEST)
   }
 
   async ListarUsuarios() {
-    // try {
-    //   const usuariosCadastrados = await usuarios.findMany({
-    //     select: {
-    //       id: true,
-    //       nome: true,
-    //       sobrenome: true,
-    //       email: true,
-    //       cargo: { select: { id: true, cargo: true } },
-    //       dt_criacao: true
-    //     }
-    //   })
-
-    //   if (!usuariosCadastrados) {
-    //     return "Não existe nenhum usuário cadastrado no sistema."
-    //   }
-
-    //   return usuariosCadastrados
-
-    // } catch (error) {
-    //   return "Erro interno. Tivemos um erro ao tentar buscar as informações dos usuários no sistema. Por favor, tente novamente."
-    // }
-
     const usuariosCadastrados = await this.prisma.usuarios.findMany({
       select: {
         id: true,
@@ -81,38 +55,6 @@ export class UsuariosService {
   }
 
   async UsuarioNome(nome: string) {
-    // try {
-    //   const nomeUsuario = await usuarios.findFirst({
-    //     where: { nome: { equals: nome, mode: "insensitive" } },
-    //     select: {
-    //       id: true,
-    //       nome: true,
-    //       sobrenome: true,
-    //       email: true,
-    //       cargo: { select: { id: true, cargo: true } },
-    //       dt_criacao: true
-    //     }
-    //   })
-
-    //   if (nomeUsuario) {
-    //     let dataFormatada = formatarDataISO(nomeUsuario.dt_criacao)
-
-    //     let infor = {
-    //       id: nomeUsuario.id,
-    //       nome: nomeUsuario.nome,
-    //       sobrenome: nomeUsuario.sobrenome,
-    //       email: nomeUsuario.email,
-    //       cargo: nomeUsuario.cargo.cargo,
-    //       cadastro: dataFormatada
-    //     }
-
-    //     return infor
-    //   }
-
-    //   return `Não existe usuário cadastrado com o nome: ${nome}.`
-    // } catch (error) {
-    //   return `Erro interno. Tivemos um erro ao tentar buscar as informações do usuário ${nome} no sistema. Por favor, tente novamente.`
-    // }
     const nomeUsuario = await this.prisma.usuarios.findFirst({
       where: { nome: { equals: nome, mode: "insensitive" } },
       select: {
@@ -144,38 +86,6 @@ export class UsuariosService {
   }
 
   async EditarUsuario(id: string, updateUsuarioDto: UpdateUsuarioDto) {
-    // try {
-    //   const { nome, sobrenome, email, senha, tipoCargo } = updateUsuarioDto;
-
-    //   const usuarioID = await usuarios.findFirst({ where: { id } })
-
-    //   if (usuarioID) {
-
-    //     let cargo = FormataCargo(tipoCargo) //TESTANDO FUNÇÃO DINAMICA PARA VALIDAR FUNÇÃO DO USUÁRIO
-
-    //     const usuarioEditado = await usuarios.update({
-    //       where: { id },
-    //       data: {
-    //         nome: nome === "" ? usuarioID.nome : nome,
-    //         sobrenome: sobrenome === "" ? usuarioID.sobrenome : sobrenome,
-    //         email: email === "" ? usuarioID.email : email,
-    //         senha: senha.trim() === "" ? usuarioID.senha : senha,
-    //         cargoId: !cargo ? usuarioID.cargoId : Number(cargo)
-    //       }
-    //     })
-
-    //     return {
-    //       status: "A edição foi concluída com sucesso.",
-    //       dados_antigos: usuarioID,
-    //       dados_atualizados: usuarioEditado
-    //     }
-    //   }
-
-    //   return `Não foi encontrado nenhum usuário com o ID=${id}`
-    // } catch (error) {
-    //   return "Erro interno. Não conseguimos realizar a consulta do usuário através do ID, por gentileza, tente novamente."
-    // }
-
     try {
       const usuarioID = await this.prisma.usuarios.findFirst({ where: { id } })
 
