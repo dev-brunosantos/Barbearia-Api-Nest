3ef91f4133a18caf024f833f4a15f25c18fcee8b
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-03 13:47:46 -0300

    Adicionando o modulo do prisma para realizar conxão com o banco de dados de forma correta

--- src/api/servicos/servicos.module.ts
+++ src/api/servicos/servicos.module.ts
@@ -1,8 +1,10 @@
+import { PrismaModule } from './../../prisma/prisma.module';
 import { Module } from '@nestjs/common';
 import { ServicosService } from './servicos.service';
 import { ServicosController } from './servicos.controller';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ServicosController],
   providers: [ServicosService],
 })
