474b59129887ce61dfb4e245ded2c25916f5bcb0
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-03 09:43:45 -0300

    Criando service do prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,15 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    async onModuleInit() {
+        const prismaClient = await this.$connect()
+        return prismaClient;
+    }
+    
+    async onModuleDestroy() {
+        const prismaClient = await this.$connect()
+        return prismaClient;
+    }
+}
